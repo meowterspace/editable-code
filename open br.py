2beec583bdb2b1efb2a0f0165c0586d45a42b2f5
Ankush Anshuman <ankush3107@gmail.com>
2016-10-31 19:09:43 +0530

    Improvise br.py

--- open br.py
+++ open br.py
@@ -1,5 +1,9 @@
 import webbrowser
+import selenium
+#selenium is also good resource, try that
 import time
-
+from mechanize import Browser
+br=Browser()
+br.open("www.google.com")
 time.sleep(0)
 webbrowser.open("www.facebook.com")
